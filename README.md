ee52fc66bfb85a24413e21641f883d4feb003d77
wernlia <wernlia@gmail.com>
2014-08-13 23:05:07 -0500

    Cleaned up code, added comments.

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Quote Test
 
 Quote Test homework for Code School Beta.
 
-This website presents a quote quiz. Users will be presented with a quote, and will choose between Terry Pratchett and Douglas Adams. Currently, the site only contains one quote. It will be expanded to have 12 quotes and give a final score.
+This website presents a quote quiz. Users will be presented with a quote, and will choose between Terry Pratchett and Douglas Adams. There are 12 quotes presented in a random order. The code uses a secret formula to figure out who said it.
 
 Known issues:
 1. It's kind of ugly.
--- js/myscripts.js
+++ js/myscripts.js
@@ -1,38 +1,32 @@
-//$("#startButton").on('click',function(){
-	//$("#welcomeText").hide();
-	//$(".theQuiz").show();
-	//$("#quoteOne").show();
-//	alert("working");
-//});
-var usedQuotes = [];
-var quoteIndex;
-var correctAnswers = 0;
-var quoteAnswerKey = [605, 765, 35, 1845, 285, 585, 1545, 45, 775, 1125, 1765, 1665];
+var usedQuotes = []; // keeps track of which quotes have been shown
+var quoteIndex; // contains the current quote
+var correctAnswers = 0; // keeps track of how many correct answers.
+var quoteAnswerKey = [605, 765, 35, 1845, 285, 585, 1545, 45, 775, 1125, 1765, 1665]; // Crazy answer key
 
-$(document).ready(function(){
+$(document).ready(function(){ // ready to roll
 
-  $('#startButton').click(function() {
-    $("#startButtonContainer").hide();
-    nextQuote();
+  $('#startButton').click(function() { // start button 
+    $("#startButtonContainer").hide(); // hide the button
+    nextQuote(); // get the first quote
   });  
   $('#pratchettButton').click(function(){
-    pratchettSelected();
+    pratchettSelected(); // user selected Pratchett
   });
   $('#adamsButton').click(function(){
-    adamsSelected();
+    adamsSelected(); // user selected Adams
   });
   $('#nextButton').click(function(){
-    if(usedQuotes.length < 12){
-      nextQuote();
+    if(usedQuotes.length < 12){ // if there are still quotes left,
+      nextQuote(); // grab the next one
     }
-    else theFinalAnswer();
+    else theFinalAnswer(); // no quotes left, show the score
   });
 });
 
 
-var getAQuote = function(){
-  quoteIndex = Math.floor((Math.random() * 12));
-  var theQuotes = 
+var getAQuote = function(){ // function to generate a quote and a quote index
+  quoteIndex = Math.floor((Math.random() * 12)); // random number, 0 through 11
+  var theQuotes = // array containing 12 quotes in a random order
   [
     "The trouble with having an open mind, of course, is that people will insist on coming along and trying to put things in it.                                             ",
     "I love deadlines. I like the whooshing sound they make as they fly by.                                                                                                  ",
@@ -47,75 +41,72 @@ var getAQuote = function(){
     "Everything starts somewhere, although many physicists disagree.                                                                                                         ",
     "In the beginning the Universe was created. This has made a lot of people very angry and been widely regarded as a bad move.                                             "
   ];
-  while(usedQuotes.indexOf(quoteIndex) > -1)
+  while(usedQuotes.indexOf(quoteIndex) > -1) // if the random number has already been used
   {
-    quoteIndex = Math.floor((Math.random() * 12));
+    quoteIndex = Math.floor((Math.random() * 12)); // try again
   }
-  usedQuotes = usedQuotes.concat(quoteIndex);
-  return theQuotes[quoteIndex];
+  usedQuotes = usedQuotes.concat(quoteIndex); // found a new number, add it to the list
+  return theQuotes[quoteIndex]; // return the new quote text
 
 };
 
-var theAnswerToLifeTheUniverseAndEverything = function(theQuestion){
-  debugger;
-  var theAnswer = Math.floor(Math.abs((2*(Math.sin((theQuestion*Math.PI)/360))*(Math.cos((theQuestion*Math.PI)/360))) * 60));
-  debugger;
-  return theAnswer;
+var theAnswerToLifeTheUniverseAndEverything = function(theQuestion){ // secret formula decoder
+  var theAnswer = Math.floor(Math.abs((2*(Math.sin((theQuestion*Math.PI)/360))*(Math.cos((theQuestion*Math.PI)/360))) * 60)); // what?!?!
+  return theAnswer; // give it back
 }
 
-var pratchettSelected = function(){
-  var theAnswer = theAnswerToLifeTheUniverseAndEverything(quoteAnswerKey[quoteIndex]);
-  if(theAnswer != 42){
-    showCorrect();
-    correctAnswers += 1;
+var pratchettSelected = function(){ // user clicked Pratchett
+  var theAnswer = theAnswerToLifeTheUniverseAndEverything(quoteAnswerKey[quoteIndex]); // decode the quote index
+  if(theAnswer != 42){ // this is not an Adams quote
+    showCorrect(); // so Pratchett is correct
+    correctAnswers += 1; // give the user credit
   }
-  else
+  else // this is an Adams quote
   {
-    showWrong();
+    showWrong(); // tell them they suck
   }
 }
 
-var adamsSelected = function(){
-  var theAnswer = theAnswerToLifeTheUniverseAndEverything(quoteAnswerKey[quoteIndex]);
-  if(theAnswer == 42){
-    showCorrect();
-    correctAnswers += 1;
+var adamsSelected = function(){ // user selected Adams
+  var theAnswer = theAnswerToLifeTheUniverseAndEverything(quoteAnswerKey[quoteIndex]); // decode the quote index
+  if(theAnswer == 42){ // the great answer
+    showCorrect(); // so Adams is correct
+    correctAnswers += 1; // give the user credit
   }
-  else{
-    showWrong();
+  else{ // this is a Pratchett quote
+    showWrong(); // tell them they suck
   }
 }
 
-var showCorrect = function(){
+var showCorrect = function(){ // tell them they're correct
   $("#theQuiz").hide();
   $("#buttonArea").hide();
   $("#correctArea").show();
   $("#nextButtonArea").show();
 }
 
-var showWrong = function(){
+var showWrong = function(){ // tell them they suck
   $('#theQuiz').hide();
   $("#buttonArea").hide();
   $("#wrongArea").show();
   $("#nextButtonArea").show();
 }
 
-var nextQuote = function(){
-  var newQuote = getAQuote();
-  $("#theActualQuote").text(newQuote);
+var nextQuote = function(){ // gets the next quote
+  var newQuote = getAQuote(); // get the quote
+  $("#theActualQuote").text(newQuote); // change out the text
   $("#correctArea").hide();
   $("#wrongArea").hide();
   $("#nextButtonArea").hide();
-   $("#theQuiz").slideDown("slow");
+   $("#theQuiz").slideDown("slow"); // show it
    $("#buttonArea").show();
 }
 
-var theFinalAnswer = function(){
+var theFinalAnswer = function(){ // gives the final score
   $("#correctArea").hide();
   $("#wrongArea").hide();
   $("#nextButtonArea").hide();
   $("#theQuiz").hide();
-  $("#yourScore").text(correctAnswers);
-  $("#yourScoreBox").show();
-
+  $("#yourScore").text(correctAnswers); // change out the value
+  $("#yourScoreBox").show(); // show it
 }
